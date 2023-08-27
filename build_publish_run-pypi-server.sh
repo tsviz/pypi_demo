a5b7487137354a21e329655f3463301191538dc1
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2023-08-27 22:11:03 +0000

    initial commit

---
+++ build_publish_run-pypi-server.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+python setup.py sdist
+pip install virtualenv
+mkdir ~/packages
+cd packages
+virtualenv venv
+source venv/bin/activate
+pip install pypiserver
+mv ./dist/*.tar.gz ~/packages/
+pypi-server -p 8080 ~/packages
+rm -r dist
+rm -r ~/packages
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tsvi_package',
+    packages=['tsvi_package'],
+    description='Hello world enterprise edition',
+    version='0.1',
+    url='http://github.com/example/tsvi_example',
+    author='Tsvi',
+    author_email='docs@tsviz.com',
+    keywords=['pip','tsvi','example']
+    )
